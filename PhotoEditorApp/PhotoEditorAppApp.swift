c2dec7bfe83058b1f918792b725e03bf1fa3a148
Marat Khayrullin <m.f.khayrullin@gmail.com>
2024-05-07 20:45:38 +0300

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by MAC  on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
